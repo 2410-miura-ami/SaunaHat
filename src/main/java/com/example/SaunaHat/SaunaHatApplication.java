0d26617cf5d186a4fdcb4bf59080c565b7ce233b
miura_ami <miura.ami@alhinc.jp>
2024-11-21 11:42:06 +0900

    First commit

---
+++ src/main/java/com/example/SaunaHat/SaunaHatApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaunaHat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaunaHatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaunaHatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SaunaHat/SaunaHatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SaunaHat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaunaHatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
