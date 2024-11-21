9b8b3b4aa0940be7c09a2b83b772573a00ea26b5
saito_shuhei <saito.shuhei@alhinc.jp>
2024-11-21 14:31:16 +0900

    ログイン画面編集中

---
+++ src/main/java/com/example/SaunaHat/controller/ForumController.java
@@ -0,0 +1,30 @@
+package com.example.SaunaHat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ForumController {
+
+    @GetMapping
+    public void login(){
+        ModelAndView mav = new ModelAndView();
+        // form用の空のentityを準備
+        ReportForm reportForm = new ReportForm();
+        // 画面遷移先を指定
+        mav.setViewName("/new");
+        // 準備した空のFormを保管
+        mav.addObject("formModel", reportForm);
+        //エラーメッセージ表示
+        List<String> errorMessage = (List<String>) session.getAttribute("errorMessages");
+        if (errorMessage != null){
+            mav.addObject("errorMessages", errorMessage);
+            session.invalidate();
+        }
+        return mav;
+    }
+}
